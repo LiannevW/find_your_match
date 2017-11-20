05b03edc2cebf52327e423fbe805ee263cbe6cf7
VERANIKA <veranika.isakova89@gmail.com>
2017-11-20 15:10:43 +0100

    Change seeds

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,9 @@
 class Profile < ApplicationRecord
-  
+
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :is_admin, presence: true
+  
 
   def full_name
     "#{first_name} #{last_name}"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-Profile.destroy_all
-User.destroy_all
 
-#Users
-user_1 = User.create!(email: "user1@gmail.com", password:"1234567")
-user_2 = User.create!(email: "user2@gmail.com", password:"12345678")
-user_3 = User.create!(email: "user3@gmail.com", password:"123456789")
-user_4 = User.create!(email: "user4@gmail.com", password:"1234567890")
+User.destroy_all
+Profile.destroy_all
 
 #Profiles
-profile_1 = Profile.create!(fist_name:"Marleen", last_name:"Kragting", is_admin:"", user_1)
-profile_2 = Profile.create!(fist_name:"Laura", last_name:"van West", is_admin:"", user_2)
-profile_3 = Profile.create!(fist_name:"Rudolf", last_name:"Spanneberg", is_admin:"", user_3)
-profile_4 = Profile.create!(fist_name:"Charlotte", last_name:"van der Waals", is_admin:"", user_4)
+profile_1 = Profile.create!(first_name: "Marleen", last_name: "Kragting", is_admin: true )
+profile_2 = Profile.create!(first_name: "Laura", last_name: "van West", is_admin: true )
+profile_3 = Profile.create!(first_name: "Rudolf", last_name: "Spanneberg", is_admin: false )
+profile_4 = Profile.create!(first_name: "Charlotte", last_name: "van der Waals", is_admin: false )
+#Users
+user_1 = User.create!(email: "user1@gmail.com", password: "1234567", profile: profile_1 )
+user_2 = User.create!(email: "user2@gmail.com", password: "12345678", profile: profile_2 )
+user_3 = User.create!(email: "user3@gmail.com", password: "123456789", profile: profile_3 )
+user_4 = User.create!(email: "user4@gmail.com", password: "1234567890", profile: profile_4 )
