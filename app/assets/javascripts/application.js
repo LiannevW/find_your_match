77546847f229d3dc149cab5f004f7654f0ea032c
VERANIKA <veranika.isakova89@gmail.com>
2017-11-22 15:48:43 +0100

    Change index view for matches, add javascript for the calendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,10 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(
+  function() {
+$( "#calendar_input" ).change(function() {
+  window.location = "http://localhost:3000/matches?date=2017-11-22";
+});
+});
